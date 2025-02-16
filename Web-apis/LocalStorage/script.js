c8bf0eee612d3e299542b6c203388cf043524094
Ayush Singh Tomar <ayushtomarrudra@gmail.com>
2025-02-16 23:35:39 +0530

    16_feb_2025

--- Web-apis/LocalStorage/script.js
+++ Web-apis/LocalStorage/script.js
@@ -37,3 +37,52 @@ console.log(localStorage.removeItem(1))// returns nothing undefined
 console.log(localStorage.getItem(1));
 console.log(localStorage.getItem("theme"));
 console.log(localStorage.getItem('b'));
+// localStorage.clear();
+console.log(localStorage)
+
+//looping over keys of localstorage
+console.log(localStorage.key(0))
+
+
+for(let i =0;i<localStorage.length;i++){
+    console.log(localStorage.key(i));
+    console.log(localStorage.getItem(localStorage.key(i)));
+}
+
+
+let keys= Object.keys(localStorage);
+console.log(keys);
+for(let i of keys){
+    console.log(localStorage.getItem(i))
+}
+
+// storage type stores only string data
+// but you can add data converting it into storage 
+
+const settings = {
+    background : "black",
+    color : "white",
+    theme : "dark",
+}
+
+// for(let i in settings){
+//     console.log(i,settings[i])
+//     localStorage.setItem(i, settings[i]);
+// }
+// console.log("-----------------------------")
+// for(i in settings){
+//     console.log(localStorage.getItem(i))
+// }
+
+localStorage.setItem("theme",settings);
+console.log(...localStorage.getItem("theme"));
+// it says either [ o b j e c t   O b j e c t ] or [object Object]
+
+//right way to do this is
+
+
+localStorage.setItem("theme",JSON.stringify(settings));
+console.log(localStorage.getItem("theme"));
+
+
+console.log(JSON.parse(localStorage.getItem("theme")));
