a307b2facd1b2d5598fce00a4b184eb85abdd078
Ayush Singh Tomar <ayushtomarrudra@gmail.com>
2025-02-19 21:16:57 +0530

    session_storagedata

--- Web-apis/Session Storage API/script.js
+++ Web-apis/Session Storage API/script.js
@@ -7,3 +7,13 @@ sessionStorage.setItem("Theme","dark");
 
 console.log(sessionStorage.getItem("Theme"));
 console.log(sessionStorage.getItem("IsThisFirstTime_Log_From_LiveServer"));
+
+// if you want to remove item from session storage then ,
+
+sessionStorage.removeItem("Theme");
+console.log(sessionStorage.getItem("Theme"));
+
+// if you want clear all the data from the session storage then you cane use .clear method of the session storage
+
+sessionStorage.clear();
+console.log(sessionStorage.getItem("IsThisFirstTime_Log_From_LiveServer"));
